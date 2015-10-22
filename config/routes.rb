1de424caa1fcef06fec82a39a6ae2c641ba18a5c
ramirosc <ramiro@chicagobooth.edu>
2015-10-22 17:37:04 -0500

    hw update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-get("/", :controller => "rps" , :action => "new")
+get("/", :controller => "rps" , :action => "rock")
 get("/rock", :controller => "rps" , :action => "rock")
 get("/paper", :controller => "rps" , :action => "paper")
 get("/scissors", :controller => "rps" , :action => "scissors")
